74fed06613103dce167e1ac83c924220f95dfefa
k1cker666 <pomoymu@gmail.com>
2024-01-23 18:19:55 +0300

    fix

--- TwoSum.py
+++ TwoSum.py
@@ -7,16 +7,17 @@ class TwoSum:
         self.target = target
         
     def function(self):
-        print(f"Набор чисел: {self.nums}")
-        print(f"Ожидаемая сумма: {self.target}")
-        answer = list()
-        for x in range(0, len(self.nums)):
-            for y in range(0, len(self.nums)):
-                if self.nums[x] + self.nums[y] == self.target:
-                    answer.append(x)
-                    answer.append(y)
-        answer = set(answer)
+        print(f"Набор чисел: {self.nums}")  # выводим исходный набор
+        print(f"Ожидаемая сумма: {self.target}")  # выводим ожидаемый реузльтат
+        answer = list()  # создаем список для сохранения ответов 
+        for x in range(0, len(self.nums)):  # индекс первого слагаемого
+            for y in range(0, len(self.nums)):  # индекс второго слагаемого
+                if self.nums[x] + self.nums[y] == self.target:  # сравниеваем сумму
+                    if x != y:
+                        answer.append(x)  # сохраняем в список первый индекс
+                        answer.append(y)  # сохраняем в список второй индекс 
+        answer = set(answer)  # убираем повторения в списке 
         print(f"Индексы чисел: {answer}")
         
-frst = TwoSum([3, 2, 7, 9], 10)
+frst = TwoSum([3, 2, 4], 6)
 frst.function()
