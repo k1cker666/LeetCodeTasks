1836ccd5e2db6bd85f132680a4c15f55541c28e0
k1cker666 <pomoymu@gmail.com>
2024-01-24 12:56:14 +0300

    optimized

--- TwoSum.py
+++ TwoSum.py
@@ -12,12 +12,13 @@ class TwoSum:
         answer = list()  # создаем список для сохранения ответов 
         for x in range(0, len(self.nums)):  # индекс первого слагаемого
             for y in range(0, len(self.nums)):  # индекс второго слагаемого
+                if x == y:
+                    continue
                 if self.nums[x] + self.nums[y] == self.target:  # сравниеваем сумму
-                    if x != y:
-                        answer.append(x)  # сохраняем в список первый индекс
-                        answer.append(y)  # сохраняем в список второй индекс 
+                    answer.append(x)  # сохраняем в список первый индекс
+                    answer.append(y)  # сохраняем в список второй индекс 
         answer = set(answer)  # убираем повторения в списке 
         print(f"Индексы чисел: {answer}")
         
-frst = TwoSum([2, 7, 11, 15], 9)
+frst = TwoSum([3, 2, 4], 6)
 frst.function()
