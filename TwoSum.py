d7c2d6634580ebdf8823e26ee600ff57fc8cf538
k1cker666 <pomoymu@gmail.com>
2024-01-26 16:53:31 +0300

    added 2nd vers

--- TwoSum.py
+++ TwoSum.py
@@ -9,16 +9,35 @@ class TwoSum:
     def function(self):
         print(f"Набор чисел: {self.nums}")  # выводим исходный набор
         print(f"Ожидаемая сумма: {self.target}")  # выводим ожидаемый реузльтат
-        answer = list()  # создаем список для сохранения ответов 
+        rtype = list()  # создаем список для сохранения ответов 
         for x in range(0, len(self.nums)):  # индекс первого слагаемого
             for y in range(0, len(self.nums)):  # индекс второго слагаемого
                 if x == y:
                     continue
                 if self.nums[x] + self.nums[y] == self.target:  # сравниеваем сумму
-                    answer.append(x)  # сохраняем в список первый индекс
-                    answer.append(y)  # сохраняем в список второй индекс 
-        answer = set(answer)  # убираем повторения в списке 
-        print(f"Индексы чисел: {answer}")
+                    rtype.append(x)  # сохраняем в список первый индекс
+                    rtype.append(y)  # сохраняем в список второй индекс 
+        rtype = set(rtype)  # убираем повторения в списке 
+        print(f"Индексы чисел: {rtype}")
         
-frst = TwoSum([3, 2, 4], 6)
-frst.function()
+    def function_version2(self):
+        print(f"Набор чисел: {self.nums}")
+        print(f"Ожидаемая сумма: {self.target}")
+        rtype = list()
+        for x in range(0, len(self.nums)):
+            if (self.target - self.nums[x]) in self.nums: 
+                y = self.nums.index(self.target - self.nums[x])
+                if x != y:
+                    rtype.append(x)
+                    rtype.append(y)
+        rtype = set(rtype)
+        print(rtype) 
+        
+frst = TwoSum([3, 3], 6)
+frst.function_version2()
+
+scnd = TwoSum([2, 7, 11, 15], 9)
+scnd.function_version2()
+
+thrd = TwoSum([3, 2, 4], 6)
+thrd.function_version2()
